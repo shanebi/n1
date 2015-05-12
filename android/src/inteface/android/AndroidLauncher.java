7c7dbcad4e2511e08d25be99ccb2a248909d4333
shanebi <giovanni.armede@laposte.net>
2015-05-12 19:35:31 +0200

    interfaceLandRisk
    partie graphique du jeu

---
+++ android/src/inteface/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package inteface.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import inteface.My1GdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new My1GdxGame(), config);
+	}
+}
---
+++ desktop/src/inteface/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package inteface.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import inteface.My1GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new My1GdxGame(), config);
+	}
+}
